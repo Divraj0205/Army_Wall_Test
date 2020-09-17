f0ceceffd045f2d6702f3c42c143537f74ee8a66
Divraj0205 <70948686+Divraj0205@users.noreply.github.com>
2020-09-17 22:51:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,9 @@ var damge;
 
 function setup() {
   createCanvas(1600,400);
+  thickness=random(22,83);
   speed=random(223,321);
-  weight=random(30,52);
-  thickness=random(22,83);	
+  weight=random(30,52);	
   wall=createSprite(1200,200,thickness,height/2);
   wall.shapeColor="white";
   bullet=createSprite(50, 200, 10, 5);
